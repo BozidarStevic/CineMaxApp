3595bcebdffd4fb57dcfbc98f1a63a82745a38ef
Bozidar Stevic <bozidarstevicit@gmail.com>
2023-07-17 17:46:13 +0200

    rename

--- backend/cinemax/src/main/java/com/project/cinemax/repository/CityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.cinemax.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.cinemax.model.City;
-
-
-public interface CityRepository extends JpaRepository<City, Long> {
-
-}
