fc5493524b6136cfbbb5681b81bc21021d6065be
BozidarStevic <84711641+BozidarStevic@users.noreply.github.com>
2023-08-23 15:04:55 +0200

    Delete backend (Spring Boot)/cinemax directory

--- backend (Spring Boot)/cinemax/src/main/java/com/project/cinemax/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.cinemax.repository;
-
-import com.project.cinemax.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RoleRepository extends JpaRepository<Role, String> {
-
-}
